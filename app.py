6ea56a24239491af63701306b4a2b84b5bc53e13
Jevan Ray <jevanbayedbed@gmail.com>
2025-06-02 17:31:50 +0800

    Fix: updated app.py for deployment

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ def home():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
-
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
 
