5c97756ae274d1c5e9e5da4caf61cb4b87febda6
Jevan Ray <jevanbayedbed@gmail.com>
2025-06-02 17:56:13 +0800

    Update index and layout files

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-import os
+    return "Hello, Jevan!"
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
-
