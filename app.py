53ca0e8815b40eceb39de298c1cb8bbebb1b0c72
Jevan Ray <jevanbayedbed@gmail.com>
2025-06-02 17:09:13 +0800

    Add dynamic port for Render

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 9000))  # use Render’s port or 9000 by default
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
 
 
